bf393baca367c5e67a659f014698d3845df11094
ryad boulahrouf <yoruzuya84@hotmail.fr>
2022-08-17 18:23:10 +0200

    css

--- Ticketing/models.py
+++ Ticketing/models.py
@@ -1,5 +1,3 @@
-from email.policy import default
-from multiprocessing.sharedctypes import Value
 from django.db import models
 from LITReview import settings
 from django.core.validators import MinValueValidator, MaxValueValidator
